a9b17231a8d3b4d4ea67fbb7e2dbf31a66e094c9
Estrella Ms <estrellams@Estrellas-MacBook-Pro.local>
2022-09-23 10:24:16 -0500

    jquery changes

--- functions.js
+++ functions.js
@@ -14,10 +14,31 @@ $(document).on('submit', '#schedule', function (e){
     e.preventDefault();
     $("#schedule").css({ display: "none" });
     $("#personal-info").css({ display: "flex" });
+
 });
+
+
+$( document ).ready(function() {
+    var todays_date = new Date().toLocaleDateString("en-ca");
+    console.log(todays_date);
+    $('#date').attr("min", todays_date);
+});
+
 
 $(document).on('submit', '#personal-info', function (e){
     e.preventDefault();
-    $("#personal-info").css({ display: "none" });
-    $("#success-message").css({ display: "block" });
+    var date = $('#date').val();
+    var time = $('#time').val();
+    var name = $('#fname').val();
+    var check_name = name.indexOf(" ");
+
+    if (check_name >= 0){
+        $("#personal-info").css({ display: "none" });
+        $("#success-message").css({ display: "block" }).append(date + " at " + time);
+        $("#error-box").css({ display: "none" });
+    } else {
+        $("#error-box").css({ display: "block" });
+        $("#fname").addClass('error');
+    }
+
 });
